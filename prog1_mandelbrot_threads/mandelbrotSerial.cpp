7647a541d140c9d087e90e03d3cbc123427aa039
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-04-08 16:21:34 +0800

    prog1: pass task1

--- prog1_mandelbrot_threads/mandelbrotSerial.cpp
+++ prog1_mandelbrot_threads/mandelbrotSerial.cpp
@@ -37,6 +37,7 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <stdio.h>
 
 static inline int mandel(float c_re, float c_im, int count)
 {
--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -36,7 +36,25 @@ void workerThreadStart(WorkerArgs * const args) {
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
 
+    // 1. 修改起始代码，使用两个处理器并行化曼德布罗特集的生成。具体来说，在线程 0 中计算图像的上半部分，
+    // 在线程 1 中计算图像的下半部分。这种问题分解类型被称为空间分解，因为图像的不同空间区域由不同的处理器计算。
+    if(0 == args->threadId) {
+
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, 
+            args->width, args->height, 0, args->height/2,
+            args->maxIterations, args->output);
+
+    }
+    else {
+
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, 
+            args->width, args->height, args->height/2, args->height/2,
+            args->maxIterations, args->output);
+
+    }
+
     printf("Hello world from thread %d\n", args->threadId);
+
 }
 
 //
